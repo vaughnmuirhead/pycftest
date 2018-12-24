ec2744712723316b0c2cde939405c09fa07eaa6c
vaughn <vaughn.muirhead@gmail.com>
2018-12-24 12:53:56 +1100

    Init.

---
+++ CFtest.py
@@ -0,0 +1,21 @@
+import logging
+import json
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class GetRequest():
+
+    def __init__(self, payload_dict, *args, **kwargs):
+        logger.info('Init {0}...'.format(__name__))
+        self.payload_dict = payload_dict
+        self.args = args
+
+    def get_json(self):
+        message = 'Getting json...'
+        logger.info(message)
+        js = json.dumps(self.payload_dict)
+        message = 'Returning json.'
+        logger.info(message)
+        return(js)
